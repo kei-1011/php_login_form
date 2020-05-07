a8687709bcf60186b774729388c5f8a29ab70af9
kohei <k_kojima0407@yahoo.co.jp>
2020-05-07 10:19:31 +0900

    ログイン機能、例外処理

--- public_html/login.php
+++ public_html/login.php
@@ -20,13 +20,15 @@ $app->run();
   <div class="container">
     <form action="" method="post">
     <div class="row">
-      <input type="email" name="email" id="email" placeholder="メールアドレス">
+      <input type="email" name="email" id="email" placeholder="メールアドレス" value="<?php echo isset($app->getValues()->email) ? h($app->getValues()->email) : '';?>">
     </div>
     <div class="row">
       <input type="password" name="password" id="password" placeholder="パスワード">
+      <p class="error"><?php echo h($app->getErrors('login'));?></p>
     </div>
     <input type="submit" value="ログイン" name="submit" class="btn">
     <p><a class="signup-link" href="/signup.php">サインアップ</a></p>
+    <input type="hidden" name="token" value="<?php echo h($_SESSION['token']);?>">
     </form>
   </div>
 </body>
